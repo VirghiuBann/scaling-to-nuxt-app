bd27765097ef8338da33de5ee5b24abac49aa3c0
virghiu Bann <virghiu@gmail.com>
2020-04-14 17:37:19 -0400

    SEO with vue-meta

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,21 @@
     <h1>Events</h1>
   </div>
 </template>
+<script>
+export default {
+  head() {
+    // <-- property used by vue-meta to add header tags
+    return {
+      title: 'Event Listing', // <-- For our title tag
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', // <-- for our meta description tag
+          content:
+            'Where you can find all the events taking place in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
